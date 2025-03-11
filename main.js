9f81347f651b56775fab9f054dfda85785004448
slay3r <120250863+00slay33r@users.noreply.github.com>
2025-03-11 20:20:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const hamburger = document.getElementById("hamburger");
 const navlinks = document.getElementById("navlinks");
-const navItems = document.querySelectorAll("#navlinks a"); // Sélectionne tous les liens du menu
+const navItems = document.querySelectorAll("#navlinks a");
 
-// Ouvrir/Fermer le menu au clic sur le hamburger
+
 hamburger.addEventListener("click", () => {
     navlinks.classList.toggle("navlinks-active");
 });
 
-// Fermer le menu quand un lien est cliqué
+
 navItems.forEach(link => {
     link.addEventListener("click", () => {
-        navlinks.classList.remove("navlinks-active"); // Supprime la classe pour cacher le menu
+        navlinks.classList.remove("navlinks-active");
     });
 });
